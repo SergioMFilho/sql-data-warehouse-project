75007c972a489f2d044e4ea1d4a85ebbac3c36b9
Sérgio Medeiros <110981308+SergioMFilho@users.noreply.github.com>
2026-01-13 11:42:43 -0300

    Adding view customer in gold layer

---
+++ scripts/gold/dim_customers/checking_duplicates.sql
@@ -0,0 +1,16 @@
+SELECT cst_id, COUNT(*) FROM 
+(select 
+	ci.cst_id,
+	ci.cst_key,
+	ci.cst_firstname,
+	ci.cst_marital_status,
+	ci.cst_gndr,
+	ci.cst_create_date,
+	ca.bdate,
+	ca.gen,
+	la.cntry
+from silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la ON ci.cst_key = la.cid
+) t GROUP BY cst_id
+HAVING COUNT(*) > 1
---
+++ scripts/gold/dim_customers/view_customers.sql
@@ -0,0 +1,18 @@
+CREATE VIEW gold.dim_customers AS 
+select 
+	ROW_NUMBER() OVER (ORDER BY cst_id) as customer_key,
+	ci.cst_id as customer_id,
+	ci.cst_key as customer_number,
+	ci.cst_firstname as first_name,
+	ci.cst_lastname as last_name,
+	la.cntry as country,
+	ci.cst_marital_status as marital_status,
+	CASE 
+		WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr
+		ELSE COALESCE(ca.gen, 'n/a')
+	END as gender,
+	ca.bdate as birthdate,
+	ci.cst_create_date as create_date
+from silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la ON ci.cst_key = la.cid
